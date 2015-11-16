d3deea81a2169154336d6844a99a645ec12b3722
Alvin Francis <nivlafx@gmail.com>
2015-11-16 19:50:23 -0400

    insignal
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_USES_BLOCK_BASED_OTA := false
 
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := cm
+TARGET_SLSI_VARIANT := insignal
 TARGET_SOC := exynos5410
 
 # Architecture
