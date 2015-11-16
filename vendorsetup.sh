499f053c101f80df392f6cee50bcfd12528a0e5f
Alvin Francis <nivlafx@gmail.com>
2015-11-16 14:56:08 -0400

    NFX
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_i9500-user
-add_lunch_combo cm_i9500-userdebug
-add_lunch_combo cm_i9500-eng
+add_lunch_combo nfx_i9500-user
+add_lunch_combo nfx_i9500-userdebug
+add_lunch_combo nfx_i9500-eng
